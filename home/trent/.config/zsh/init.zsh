31424aa30787752e0186944194b2b95f31fa2fe8
Trent <ghastfilms613@gmail.com>
2022-05-28 21:07:52 -0500

    fixed zsh conf

--- home/trent/.config/zsh/init.zsh
+++ home/trent/.config/zsh/init.zsh
@@ -1,15 +1,15 @@
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.
-source p10k.zsh
+source ./p10k.zsh
 
 autoload -Uz compinit promptinit
 compinit
 promptinit
 
 # plugins
-source plugins.zsh
+source ./plugins.zsh
 # aliases
-source aliases.zsh
+source ./aliases.zsh
 # zstyle
-source zstyle.zsh
+source ./zstyle.zsh
